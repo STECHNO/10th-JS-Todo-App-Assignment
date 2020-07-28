29878685b8b57ac61e42213e8956adf7198301c8
Saleheen Noor <saleheen.noor@gmail.com>
2020-07-29 03:58:03 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,67 +9,59 @@ c.innerHTML = b
 
 
 
+function addTask() {
+    var getTodo = document.getElementById('add-task');
+    var subTodo = document.createElement("div");
+    subTodo.setAttribute("id", "single-div");
+    subTodo.classList.add("todo-design");
+
+
+    var list = document.createElement("input");
+    list.setAttribute("type", "text");
+    list.setAttribute("value", getTodo.value);
+    list.setAttribute("id", "show");
+    list.classList.add("type-text-task");
+    list.disabled = true;
+    subTodo.appendChild(list);
+
+
+    var delBtn = document.createElement("i");
+    delBtn.classList.add("fa");
+    delBtn.classList.add("fa-trash");
+    delBtn.setAttribute("aria-hidden", "true");
+    delBtn.setAttribute("onClick", "deleteSingle(this)");
+    subTodo.appendChild(delBtn);
 
 
 
+    var editBtn = document.createElement("i");
+    editBtn.classList.add("fa");
+    editBtn.classList.add("fa-pencil-square-o");
+    editBtn.setAttribute("aria-hidden", "true");
+    editBtn.setAttribute("onClick", "editBtn(this)");
+    editBtn.setAttribute("id", "btnCycle");
+    subTodo.appendChild(editBtn);
 
+    var container = document.getElementById("todoitems");
 
-
-
-
-
-
-function addTask(){
-    var getTask = document.getElementById("add-task");
-
-    var list = document.createElement("p");
-    var listItem = document.createTextNode(getTask.value);
-    
-    list.appendChild(listItem);
-
-
-    
-
-
-
-    var delButton = document.createElement("i");
-    delButton.className = "fa fa-trash fa-2x";
-    delButton.setAttribute("aria-hidden", "true");
-
-    list.appendChild(delButton);
-
-    var editButton = document.createElement("i");
-    editButton.className = "fa fa-pencil fa-2x";
-    editButton.setAttribute("aria-hidden", "true");
-
-    list.appendChild(editButton);
-
-
-   
-
-
-
-    todoitems.appendChild(list)
+    if (document.getElementById("add-task").value == "") {
+        alert("Please Enter Any Todo..");
+    }
+    else {
+        container.appendChild(subTodo);
+        getTodo.value = "";
+    }
 }
 
+function editBtn(edit) {
+    edit.parentNode.firstChild.disabled = !edit.parentNode.firstChild.disabled;
+}
 
+function deleteSingle(d) {
+    d.parentNode.remove();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function deleteAll(){
-
+function deleteAll() {
+    var deleteAll = document.getElementById("todoitems");
+    deleteAll.innerHTML = "";
 }
